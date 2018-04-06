f25b54b024f57b494917ae8cfe2b598ca905ece7
Jamshid Ajam <mr.jamshid.a@gmail.com>
2018-04-06 10:28:16 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = {
-    token: '522963497:AAHqxjkzZOkc-LCnfZ4Naz9dHcjcImcK4GA',
+    token: '522963497:AAHqxjkzZOkc-LCnfZ4Naz9dHcjcImcK4GAA',
     botOption: {
         autoUpdate: true,
         autoChatAction: true,
